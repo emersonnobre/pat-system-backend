8dda400ea67166d65c9be1fee025722769b18a94
emersonnobre <egrnobre@gmail.com>
2024-07-25 00:55:15 -0400

    feature(api): improving get processes endpoint
    - New filter method created to isolate the filter logic;
    - Date prescription date filter added;
    - Improving response with 404 status code.

--- src/service/implementation/process.service.ts
+++ src/service/implementation/process.service.ts
@@ -4,11 +4,11 @@ import IProcessService from "../interface/i.process.service"
 import ProcessRepository from "../../repository/implementation/process.repository"
 import IProcessRepository from "../../repository/interface/i.process.repository"
 import ProcessMapper from "../../util/mapping/process.mapper"
-import ProcessResponse from "../../crossCutting/response/process/process.response"
 import ProcessShortResponse from "../../crossCutting/response/process/process-short.response"
 import PaginationFilterRequest from "../../crossCutting/request/comum/pagination.filter.request"
 import GetProcessesFilterRequest from "../../crossCutting/request/process/get-processes.filter.request"
 import PaginatedResponse from "../../crossCutting/response/comum/paginated.response"
+import Process from "../../model/process"
 import "../../util/prototypes/array.prototypes"
 
 @injectable()
@@ -20,22 +20,32 @@ export default class ProcessService implements IProcessService {
 
   get(filters: PaginationFilterRequest<GetProcessesFilterRequest>): ApiResponse<PaginatedResponse<ProcessShortResponse[]>> {
     const { filter } = filters
-    let processes = this._processRepository.get()
+    const processes = this._processRepository.get()
 
-    if (filter.executedName)
-      processes = processes.filter(process => process.executed.includes(filter.executedName || ""))
+    let filteredProcesses = this.filter(processes, filter)
+    const totalCount = filteredProcesses.length
 
-    if (filter.judgeName)
-      processes = processes.filter(process => process.judge_name.includes(filter.judgeName))
+    const paginatedResponse: PaginatedResponse<ProcessShortResponse[]> = { limit: filters.limit, offset: filters.offset, totalCount, data: undefined }
 
-    // if (filter.prescriptionDateStart) TODO: implementar comparacao das datas do filtro
-    //   processes = processes.filter(process => process.debt_amount >= filter.debtAmountMax)
+    if (totalCount == 0)
+      return { success: false, httpStatusCode: 404, data: paginatedResponse }
 
-    const totalCount = processes.length
-
-    processes = processes.skip(filters.offset).take(filters.limit)
-    const mappedProcesses = processes.map(process => this._processMapper.modelToProcessShortResponse(process))
-    const paginatedResponse: PaginatedResponse<ProcessShortResponse[]> = { limit: filters.limit, offset: filters.offset, totalCount, data: mappedProcesses } 
+    filteredProcesses = filteredProcesses.skip(filters.offset).take(filters.limit)
+    const mappedProcesses = filteredProcesses.map(process => this._processMapper.modelToProcessShortResponse(process))
+    paginatedResponse.data = mappedProcesses
+    
     return { success: true, httpStatusCode: 200, data: paginatedResponse }
+  }
+
+  private filter(processes: Process[], filters: GetProcessesFilterRequest) {
+    if (filters.executedName)
+      processes = processes.filter(process => process.executed.includes(filters.executedName || ""))
+    if (filters.judgeName)
+      processes = processes.filter(process => process.judge_name.includes(filters.judgeName))
+    if (filters.prescriptionDateStart)
+      processes = processes.filter(process => new Date(process.prescription_date) >= filters.prescriptionDateStart)
+    if (filters.prescriptionDateEnd)
+      processes = processes.filter(process => new Date(process.prescription_date) <= filters.prescriptionDateEnd)
+    return processes
   }
 }
